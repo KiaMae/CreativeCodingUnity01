05cc0b3647ea3c1c1a033b7ea396f1e2a8a559da
KiaMae <Kia-Mae@wifi-172-23-48-22.uoa-wifi.auckland.ac.nz>
2016-05-23 12:24:29 +1200

    Adding public fields
    All of these public functions are set before unity even starts the
    program up, and the functions are available throughout the code.

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,1 @@
+	public float rotateSpeed = 1.0f;
---
+++ Assets/Sketch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sketch : MonoBehaviour {
+	public GameObject myPrefab;
+
+	void Start () {
+		
+		int totalCubes = 8;
+		int totalDistance = 5;
+	
+	//Sin Distro
+		for (int i = 0; i < totalCubes; i++)
+		{
+			float perc = i / (float)totalCubes;
+			float sin = Mathf.Sin (perc * Mathf.PI/2);
+
+			float x = 2.0f + sin * totalDistance;
+			float y = 5.0f;
+			float z = 0.0f;
+
+			var newCube = (GameObject)Instantiate (myPrefab, new Vector3 (x, y, z), Quaternion.identity);
+			newCube.GetComponent<CubeScript>().SetSize(1.0f - perc);	
+			newCube.GetComponent<CubeScript> ().rotateSpeed = perc;
+		}
+	}
+	void Update () {
+	
+	}
+}
