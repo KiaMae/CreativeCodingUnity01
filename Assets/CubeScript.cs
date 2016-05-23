849284dcc487930c4e8e86073e3e80498b0c9c59
KiaMae <Kia-Mae@wifi-172-23-48-22.uoa-wifi.auckland.ac.nz>
2016-05-23 12:28:53 +1200

    The Cube
    Setting the spin of the cube to a certain speed, in this case all parts
    of the vector are randomly created by the computer

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -1,2 +1,7 @@
 using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
 	public float rotateSpeed = 1.0f;
+		spinSpeed = new Vector3 (Random.value, Random.value, Random.value);
